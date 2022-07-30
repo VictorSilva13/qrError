2e03ac78d905d94ce1393d781644311652c198f6
VictorSilva13 <augusto13victor@outlook.com>
2022-07-29 22:26:41 -0300

    jframe added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import javax.swing.*;  
 /**
  * main
  */
-public class Main {
+public class Main extends JFrame{
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        //MAIN FRAME
+        JFrame frame = new JFrame("Main window");
+        JPanel panel1 = new JPanel();
+        JLabel label1 = new JLabel("Tell us the word that you want to convert into QR Code: ");
+        JTextField txt1 = new JTextField("TYPE IN HERE :)");              
+        
+        panel1.add(label1);
+        
+        panel1.add(txt1);
+
+        frame.add(panel1);
+        
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        frame.setSize(800, 400);
+
+        frame.setVisible(true);
     }
     
 }
